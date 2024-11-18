3adb3fc1fe736c52bb6ee06de5d42ffa5f7650f5
Joseph Adams <josephdadams@gmail.com>
2024-11-18 13:06:40 -0500

    add additional model

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,12 @@ module.exports = {
 	MODELS: [
 		{ id: '250', label: 'WB-250-IPW-2', protocol: 'telnet', outlets: 2 },
 		{ id: '300', label: 'WB-300-IP-3', protocol: 'http', outlets: 3 },
+		{
+			id: '300vb',
+			label: 'WattBox WB-300VB-IP-5 300 Series IP Power Conditioner (VersaBox)',
+			protocol: 'http',
+			outlets: 5,
+		},
 		{ id: '700', label: 'WB-700-IPV-12', protocol: 'http', outlets: 12 },
 		{ id: 'other', label: 'Other' },
 	],
